943e3e9673969cfe0ddd4979381e25275eeb749e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-22 18:03:01 +0200

    individual cube making (better for diagnostics, I hope)

---
+++ reduction/individual_cube_making.py
@@ -0,0 +1,44 @@
+import socket
+
+if 'eso-macbook' in socket.gethostname():
+    execfile('run_pipeline_cyg.py')
+elif 'cleese' in socket.gethostname():
+    execfile('run_pipeline_cleese.py')
+else:
+    raise ValueError("Machine {0} not recognized.".format(socket.gethostname()))
+
+
+try:
+    label = "_v"+subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+except CalledProcessError:
+    label = ""
+
+logfile = ("".join([time.strftime("apexcmzpipeline{0}_%y_%m_%d_%H:%M:%S"),".log"])).format(label)
+
+with log.log_to_file(logfile):
+    for dataset in make_apex_cubes.datasets_ao:
+        make_apex_cubes.build_cube_ao(window='high',
+                                  datasets=[dataset],
+                                  datapath=make_apex_cubes.aorawpath,
+                                  timewise_pca=True,
+                                  pca_clean=True,
+                                  freq=True,
+                                  )
+
+    for dataset in make_apex_cubes.datasets_2014:
+        mapnames = make_apex_cubes.datasets_2014[dataset]
+
+        build_cube_2014(mapnames,
+                        datapath=make_apex_cubes.april2014path,
+                        lowhigh='low',
+                        pca_clean=True,
+                        pcakwargs={},
+                        datasets=[dataset])
+
+    for dataset in make_apex_cubes.datasets_2013:
+
+        build_cube_2013(datapath=make_apex_cubes.june2013path,
+                        lowhigh='low',
+                        pca_clean=True,
+                        pcakwargs={},
+                        datasets=[dataset])
---
+++ reduction/run_pipeline_cyg.py
@@ -0,0 +1,5 @@
+import make_apex_cubes
+from os.path import join
+from astropy import log
+import time
+import subprocess
