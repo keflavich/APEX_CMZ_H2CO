5e0844a68a46969c873984f8723b7dc1b709309f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-24 16:40:09 +0200

    cleese pipeline: do everything.

--- reduction/run_pipeline_cleese.py
+++ reduction/run_pipeline_cleese.py
@@ -24,6 +24,9 @@ except CalledProcessError:
 
 logfile = ("".join([time.strftime("apexcmzpipeline{0}_%y_%m_%d_%H:%M:%S"),".log"])).format(label)
 
+with log.log_to_file(logfile.replace(".log","_default.log")):
+   make_apex_cubes.do_everything()
+
 # with log.log_to_file(logfile.replace(".log","_default.log")):
 #    make_apex_cubes.make_high_mergecube(mergefile2='APEX_H2CO_merge_high_default')
 # with log.log_to_file(logfile.replace(".log","_nopca.log")):
