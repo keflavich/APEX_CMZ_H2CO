e83ffdb2c4e6d6eec6a32c85a91b431a51e7dcad
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-10-16 09:18:58 +0200

    pvslice fails.

--- analysis/glue_cat.py
+++ analysis/glue_cat.py
@@ -3,11 +3,20 @@ from glue.core.data_factories import astropy_tabular_data, load_data
 from glue.core.link_helpers import LinkSame, LinkTwoWay
 from glue.qt.glue_application import GlueApplication
 from glue.qt.widgets import ScatterWidget, ImageWidget
-
+from glue.qt.widgets.image_widget import (_slice_from_path, _slice_label,
+                                          _slice_index, StandaloneImageWidget,
+                                          PVSliceWidget)
 from glue import qglue
 
+from astropy import units as u
+from astropy import coordinates
+from astropy import wcs
+from astropy.io import ascii
+from paths import mpath,apath,fpath,molpath,hpath
+import pvextractor
 
-catalog = astropy_tabular_data('/Users/adam/work/apex_cmz_h2co/analysis/fitted_line_parameters_Chi2Constraints.ipac',
+
+catalog = astropy_tabular_data(apath('fitted_line_parameters_Chi2Constraints.ipac'),
                                format='ascii.ipac')
 catalog.label='FittedLineParameters'
 catalog.style.color = 'r'
@@ -46,10 +55,18 @@ scatter.xatt = catalog.id['higaldusttem']
 #scatter.xflip = True
 #scatter.ylog = True
 
-viewer = app.new_data_viewer(ImageWidget)
-viewer.add_data(cube)
-#viewer.add_subset(subset_tem_lt_60)
-#viewer.add_subset(subset_tem_gt_60)
-#viewer.add_data(catalog)
+cube_viewer = app.new_data_viewer(ImageWidget)
+cube_viewer.add_data(cube)
+#cube_viewer.add_subset(subset_tem_lt_60)
+#cube_viewer.add_subset(subset_tem_gt_60)
+#cube_viewer.add_data(catalog)
+
+table = ascii.read(apath('orbit_K14_2.dat'), format='basic', comment="#", guess=False) 
+coords = coordinates.SkyCoord(table['l']*u.deg, table['b']*u.deg, frame='galactic')[:10]
+P = pvextractor.Path(coords)
+pv = pvextractor.extract_pv_slice(cube.data['PRIMARY'], P, wcs=cube.data.coords.wcs)
+pvwidget = PVSliceWidget(image=pv.data, wcs=wcs.WCS(pv.header),
+                         image_widget=cube_viewer, interpolation='nearest')
+
 
 app.start()
