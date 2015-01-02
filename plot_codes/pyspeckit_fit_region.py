7df09a06a14e8a9cd7778f0c1c89d2c514e3964f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-01-02 10:10:13 +0100

    added pyspeckit region fitting figures

---
+++ plot_codes/pyspeckit_fit_region.py
@@ -0,0 +1,1 @@
+../analysis/pyspeckit_fit_region.py
---
+++ plot_codes/pyspeckit_region_figures.py
@@ -0,0 +1,8 @@
+import matplotlib
+from pyspeckit_fit_region import (do_1comp_region, do_g047_box,
+                                  do_pyspeck_fits_1comp, do_pyspeck_fits_2comp,
+                                  do_the_brick, get_subregion_pcube,
+                                  remove_bad_pars)
+
+cm = matplotlib.cm.RdYlBu_r
+cm.set_bad('#888888')
