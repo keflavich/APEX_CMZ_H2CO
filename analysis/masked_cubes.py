35f8ac20ec5ef830f264ba51bcb5df5af72b24d6
Adam Ginsburg <keflavich@yahoo.com>
2015-02-19 15:26:22 +0100

    mask out HC3N features

--- analysis/masked_cubes.py
+++ analysis/masked_cubes.py
@@ -8,20 +8,43 @@ from astropy import log
 
 t0 = time.time()
 
+hc3n_regions = [{'v':(-101,55),
+                 'x':(500,533),
+                 'y':(108,133),},
+                {'v':(-133,-70),
+                 'x':(787,884),
+                 'y':(87,120),}]
+
+def mask_out_region(mask_array, cube, regions=hc3n_regions):
+
+    for region in regions:
+        z = [cube.closest_spectral_channel(v*u.km/u.s)
+             for v in region['v']]
+        view = [slice(*z),
+                slice(*region['y']),
+                slice(*region['x'])
+               ]
+
+        mask_array[view] = False
+
+    return mask_array
+
 cube303 = SpectralCube.read(hpath('APEX_H2CO_303_202_bl.fits')).with_spectral_unit(u.km/u.s, velocity_convention='radio')
 cube321 = SpectralCube.read(hpath('APEX_H2CO_321_220_bl.fits')).with_spectral_unit(u.km/u.s, velocity_convention='radio')
-mask = (fits.getdata(hpath('APEX_H2CO_303_202_bl_mask.fits')).astype('bool') &
-        cube303.mask.include(cube303._data, cube303.wcs) &
-        cube321.mask.include(cube321._data, cube321.wcs))
+maskarr = mask_out_region(fits.getdata(hpath('APEX_H2CO_303_202_bl_mask.fits')).astype('bool'), cube303)
+mask = (maskarr &
+        cube303.mask.include(cube303, cube303.wcs) &
+        cube321.mask.include(cube321, cube321.wcs))
 bmask = BooleanArrayMask(mask, cube303.wcs)
 cube303m = cube303.with_mask(bmask)
 cube321m = cube321.with_mask(bmask)
 
 cube303sm = SpectralCube.read(hpath('APEX_H2CO_303_202_smooth_bl.fits')).with_spectral_unit(u.km/u.s, velocity_convention='radio')
 cube321sm = SpectralCube.read(hpath('APEX_H2CO_321_220_smooth_bl.fits')).with_spectral_unit(u.km/u.s, velocity_convention='radio')
-masksm = (fits.getdata(hpath('APEX_H2CO_303_202_smooth_bl_mask.fits')).astype('bool') &
-          cube303sm.mask.include(cube303sm._data, cube303sm.wcs) &
-          cube321sm.mask.include(cube321sm._data, cube321sm.wcs))
+smmaskarr = mask_out_region(fits.getdata(hpath('APEX_H2CO_303_202_smooth_bl_mask.fits')).astype('bool'), cube303sm)
+masksm = (smmaskarr &
+          cube303sm.mask.include(cube303sm, cube303sm.wcs) &
+          cube321sm.mask.include(cube321sm, cube321sm.wcs))
 bmasksm = BooleanArrayMask(masksm, cube303sm.wcs)
 cube303msm = cube303sm.with_mask(bmasksm)
 cube321msm = cube321sm.with_mask(bmasksm)
