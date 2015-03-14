81a5464efa67d8d9c9f1639291390212004a1fca
Adam Ginsburg <keflavich@yahoo.com>
2015-03-14 15:22:35 +0100

    more shock discussion and updated title

--- plot_codes/yt_meshes.py
+++ plot_codes/yt_meshes.py
@@ -1,15 +1,22 @@
 import yt
 import paths
+from temperature_cubes import tcube,tcube_direct
+from masked_cubes import cube303m
+from spectral_cube import SpectralCube
+
+tcube_direct._header['BTYPE'] = 'Temperature'
+tcube_direct._header['BUNIT'] = 'K'
+cube303m._header['BTYPE'] = 'flux'
+hdulist = fits.HDUList([cube303m.hdu, tcube_direct.hdu])
+hdu  = cube303m.hdu
+hdu.data = np.rollaxis(np.array([cube303m.hdu.data, tcube_direct.hdu.data]),0,3)
 
 
+ytds = yt.load(fits.HDUList(hdu), nan_mask=0.0, z_axis_decomp=True)
 
-ytds_sm = yt.load(paths.hpath('APEX_H2CO_303_202_smooth_bl_snmasked.fits'),
-               auxiliary_files=[paths.hpath('H2CO_321220_to_303202_cube_smooth_bl_temperature.fits')],
-               nan_mask=0.0, z_axis_decomp=True)
+ytds.periodicity = (True,)*3
 
-ytds_sm.periodicity = (True,)*3
-
-surface = ytds_sm.surface(ytds_sm.all_data(), ytds_sm.field_list[0], 0.2)
+surface = ytds.surface(ytds.all_data(), ytds.field_list[0], 0.2)
 
 surface.export_sketchfab(title='H2CO brightness colored by temperature (smooth)',
                          description='none', color_field='tkin',
