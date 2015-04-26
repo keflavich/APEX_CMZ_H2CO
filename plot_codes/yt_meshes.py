de9a250b9418e923ee2fbe7bd8fc3b62c306e883
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-26 11:09:48 +0200

    factor out some unnecessary mesh lines.  Unfortunately, still doesn't work

--- plot_codes/yt_meshes.py
+++ plot_codes/yt_meshes.py
@@ -25,21 +25,20 @@ ytds = yt.frontends.fits.FITSDataset(hdu1, auxiliary_files=[hdu2])
 
 ytds.periodicity = (True,)*3
 
-surface = ytds.surface(ytds.all_data(), ytds.field_list[0], 0.2)
+surface = ytds.surface(ytds.all_data(), ('fits','brightness'), 0.2)
 
-surface.export_sketchfab(title='H2CO brightness colored by temperature (smooth)',
+surface.export_sketchfab(title='H2CO brightness colored by temperature',
                          description='none', color_field='tkin',
                          color_log=False, color_map='Blue-Red',
                          #color_field_min=20, color_field_max=300,
                         )
 
-ytds = yt.load(paths.hpath('APEX_H2CO_303_202_bl_snmasked.fits'),
-               auxiliary_files=[paths.hpath('H2CO_321220_to_303202_cube_bl_temperature.fits')],
-               nan_mask=0.0, z_axis_decomp=True)
-
-ytds.periodicity = (True,)*3
-
-surface = ytds.surface(ytds.all_data(), ytds.field_list[0], 0.2)
+surface.export_obj('h2co_temperatures'.format(level),
+                   color_field='tkin', color_log=False,
+                   color_map='Blue-Red',
+                   #color_field_min=20, color_field_max=300,
+                   #transparency=0.5,
+                  )
 
 surface.export_sketchfab(title='H2CO brightness colored by temperature',
                          description='none', color_field='tkin',
@@ -50,15 +49,9 @@ surface.export_sketchfab(title='H2CO brightness colored by temperature',
 for level in (0.1,0.2,0.3,0.4,0.5,0.6):
 
 
-    ytds_sm = yt.load(paths.hpath('APEX_H2CO_303_202_smooth_bl_snmasked.fits'),
-                   auxiliary_files=[paths.hpath('H2CO_321220_to_303202_cube_smooth_bl_temperature.fits')],
-                   nan_mask=0.0, z_axis_decomp=True)
+    surface = ytds.surface(ytds_sm.all_data(), ytds_sm.field_list[0], level)
 
-    ytds_sm.periodicity = (True,)*3
-
-    surface = ytds_sm.surface(ytds_sm.all_data(), ytds_sm.field_list[0], level)
-
-    surface.export_ply('h2co_smooth_temperatures_cut{0:f}.ply'.format(level),
+    surface.export_ply('h2co_temperatures_cut{0:f}.ply'.format(level),
                        color_field='tkin', color_log=False,
                        color_map='Blue-Red',
                        #color_field_min=20, color_field_max=300,
