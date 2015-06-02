c1f0114682ad9e3d2490552e5e07eb1c3aa80389
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-02 07:47:40 -0400

    figure caption correction since colors changed

--- analysis/constrain_parameters.py
+++ analysis/constrain_parameters.py
@@ -310,11 +310,11 @@ class paraH2COmodel(generic_paraH2COmodel):
             cdict = {x:   [(0.0, 0.0, 0.0),
                            (1.0, 0.0, 0.0)]
                      for x in ('red','green','blue')}
-            cdict['alpha'] = [(0.0, 0.0, 0.0), (1.0, 0.2, 0.2)]
+            cdict['alpha'] = [(0.0, 0.0, 0.0), (1.0, 0.3, 0.3)]
             darker = matplotlib.colors.LinearSegmentedColormap('darker', cdict)
             like = (self.individual_likelihoods['chi2_ff1'])
             plim = cdf_of_like(like.sum(axis=axis)).swapaxes(*swaps)
-            pl.contourf(xax, yax, plim, levels=levels,
+            pl.contourf(xax, yax, plim, levels=levels+[1],
                         cmap=darker, zorder=5)
         if hasattr(self.chi2_dens, 'size'):
             like = (self.individual_likelihoods['chi2_dens'])
