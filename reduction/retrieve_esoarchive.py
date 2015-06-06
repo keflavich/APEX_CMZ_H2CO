1b0a9fba6c829dfdc30bc0a65915eb4f1d22f2b7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-06 17:22:57 -0400

    updates to ESO archive retrieval code: broken on the astropy side (within astroquery)

--- reduction/retrieve_esoarchive.py
+++ reduction/retrieve_esoarchive.py
@@ -1,7 +1,11 @@
 from astroquery.eso import Eso
 import paths
 
-def retrieve_ESO_files(rawpath='/scratch/aginsbur/apex/raw/', username='aginsburg',):
+def retrieve_ESO_files(rawpath='/scratch/aginsbur/apex/raw/',
+                       username='aginsburg', projids=['O-085.F-9311A',
+                                                      'E-085.B-0964A',
+                                                      'E-093.C-0144A',
+                                                      'E-095.C-0242A']):
 
     # don't touch the rest
     Eso.cache_location = rawpath
@@ -9,13 +13,12 @@ def retrieve_ESO_files(rawpath='/scratch/aginsbur/apex/raw/', username='aginsbur
     Eso.login(username)
 
     Eso.ROW_LIMIT = 1000000
-    tbl = Eso.query_instrument('apex', pi_coi='ginsburg', cache=False)
+    #tbl = Eso.query_instrument('apex', pi_coi='ginsburg', cache=False)
     #stbl = tbl[np.char.startswith(tbl['Object'], 'Map') & (tbl['Scan Mode']=='OTF') & (tbl['Number of subscans'] > 10)]
     #programs = set(tbl['ProgId'])
-    projids = set(tbl['APEX Project ID'])
+    #projids = set(tbl['APEX Project ID'])
 
     for proj in projids:
-        tbl = Eso.query_apex_quicklooks(proj[:-5])
+        tbl = Eso.query_apex_quicklooks(proj)
         print(tbl)
-        if proj[-4:] == '2015':
-            files = Eso.retrieve_data(tbl['Product ID'])
+        #files = Eso.retrieve_data(tbl['Product ID'])
