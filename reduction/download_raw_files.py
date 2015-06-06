6e6b21386439ab54df7ba4776922bf2c80a418e6
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-06-06 15:23:26 -0400

    add a download_raw_files script!   Oooooh!  ahhhh!!

---
+++ reduction/download_raw_files.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import os
+import retrieve_esoarchive
+import cgi
+import requests
+from astropy.utils.data import download_files_in_parallel
+from astropy.utils.console import ProgressBar
+
+rawpath='/scratch/aginsbur/apex/raw/'
+eso_username='aginsburg'
+
+mpi_raw_files = ['https://dataverse.harvard.edu/api/v1/access/datafile/2509801',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2504422',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509797',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509803',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509790',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2504424',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2504425',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2505538',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509798',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2505539',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509802',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509804',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509800',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2509799',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2505540',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2510176',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2505541',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2510177',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2505542',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2504384',
+                 'https://dataverse.harvard.edu/api/v1/access/datafile/2504383']
+
+def download_file(url, outdir=rawpath):
+    r = requests.get(url, verify=False, stream=True)
+    _, cdisp = cgi.parse_header(r.headers['content-disposition'])
+    outfilename = cdisp['filename']
+    fullname = os.path.join(outdir, outfilename)
+
+    pb = ProgressBar(int(r.headers['content-length']))
+
+    with open(fullname, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            f.write(chunk)
+            f.flush()
+            pb.update(pb._current_value + 1024)
+
+    return fullname
+
+def download_all():
+
+    retrieve_esoarchive.retrieve_ESO_files(rawpath=rawpath, username=eso_username)
+
+    for ii,fn in enumerate(mpi_raw_files):
+        download_file(fn)
+        print("Completed {0} of {1} files".format(ii, len(mpi_raw_files)))
--- reduction/retrieve_esoarchive.py
+++ reduction/retrieve_esoarchive.py
@@ -1,23 +1,21 @@
 from astroquery.eso import Eso
 import paths
 
-# configure this
-rawpath = '/scratch/aginsbur/apex/raw/'
-username = 'aginsburg'
+def retrieve_ESO_files(rawpath='/scratch/aginsbur/apex/raw/', username='aginsburg',):
 
-# don't touch the rest
-Eso.cache_location = rawpath
+    # don't touch the rest
+    Eso.cache_location = rawpath
 
-Eso.login(username)
+    Eso.login(username)
 
-Eso.ROW_LIMIT = 1000000
-tbl = Eso.query_instrument('apex', pi_coi='ginsburg', cache=False)
-#stbl = tbl[np.char.startswith(tbl['Object'], 'Map') & (tbl['Scan Mode']=='OTF') & (tbl['Number of subscans'] > 10)]
-#programs = set(tbl['ProgId'])
-projids = set(tbl['APEX Project ID'])
+    Eso.ROW_LIMIT = 1000000
+    tbl = Eso.query_instrument('apex', pi_coi='ginsburg', cache=False)
+    #stbl = tbl[np.char.startswith(tbl['Object'], 'Map') & (tbl['Scan Mode']=='OTF') & (tbl['Number of subscans'] > 10)]
+    #programs = set(tbl['ProgId'])
+    projids = set(tbl['APEX Project ID'])
 
-for proj in projids:
-    tbl = Eso.query_apex_quicklooks(proj[:-5])
-    print(tbl)
-    if proj[-4:] == '2015':
-        files = Eso.retrieve_data(tbl['Product ID'])
+    for proj in projids:
+        tbl = Eso.query_apex_quicklooks(proj[:-5])
+        print(tbl)
+        if proj[-4:] == '2015':
+            files = Eso.retrieve_data(tbl['Product ID'])
