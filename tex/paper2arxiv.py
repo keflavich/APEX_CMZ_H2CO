79b29be2634da8cb7587366e004a31e92398befe
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-10 15:47:26 +0100

    py3ify the paper2arxiv script, and prep it for aanda

--- tex/paper2arxiv.py
+++ tex/paper2arxiv.py
@@ -1,29 +1,35 @@
+from __future__ import print_function
 import re,os,time
 import argparse
 import shutil
 import ipdb
+from six import string_types as basestring
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--reconvert",default=False,action='store_true')
-parser.add_argument("--arxiv",default=True,action='store_true')
+#parser.add_argument("--arxiv",default=True,action='store_true')
+parser.add_argument("--aanda",default=False,action='store_true')
 parser.add_argument("--texit",default=False,action='store_true')
 args = parser.parse_args()
-print "ARGS: ",args
+print("ARGS: ",args)
 
 ppath='/Users/adam/work/apex_cmz_h2co/tex/'
 paper_name = 'apex_cmz_h2co'
 file = open(os.path.join(ppath,paper_name+'.tex'),'r')
 
-outdir = "arxiv/" if not args.arxiv else "arxiv/"
-outtype = "arxiv" if not args.arxiv else "arxiv"
+isarxiv = not args.aanda
+outdir = "aanda/" if not isarxiv else "arxiv/"
+outtype = "aanda" if not isarxiv else "arxiv"
+print("Converting package to {0} format".format(outtype))
 
 if not os.path.exists(ppath+outdir):
     os.mkdir(ppath+outdir)
 
-shutil.copy('aamacros.tex', 'arxiv/')
-shutil.copy('aa.cls', 'arxiv/')
+shutil.copy('aamacros.tex', outdir)
+shutil.copy('aa.cls', outdir)
 
 outfn = ppath+outtype+'form_temp.tex'
+print("Creating temporary file: {0}".format(outfn))
 outf = open(outfn,'w')
 
 inputre = re.compile('input{(.*)}')
@@ -52,20 +58,23 @@ def strip_input(list_of_lines):
 def dobib(bib, outf):
     #bn = bib.groups()[0] + '.bbl'
     bn = paper_name+".bbl"
-    print "Doing bibliography " + bn
+    print("Doing bibliography " + bn)
     with open(ppath+bn,'r') as f:
-        print >>outf,strip_input(f.readlines()),
+        print(strip_input(f.readlines()), end='', file=outf)
 
+spacecount=0
 for ii,line in enumerate(file.readlines()):
     if line[0] == "%":
         continue
+    elif line[0] == " ":
+        spacecount += 1
     input = inputre.search(line)
     include = includere.search(line)
     bib = bibre.search(line)
     solobib = solobibre.search(line)
     if solobib is not None:
         fn = solobib.groups()[0] + ".tex"
-        print ii, "Doing solobib " + fn
+        print(ii, "Doing solobib " + fn)
         with open(os.path.join(ppath,fn),'r') as f:
             solobib = f.readlines()
             for ln in solobib:
@@ -73,32 +82,34 @@ for ii,line in enumerate(file.readlines()):
                     continue
                 bib = bibre.search(ln)
                 if bib is not None:
-                    print ii,"Bib matched: ",bib.groups()
+                    print(ii,"Bib matched: ",bib.groups())
                     dobib(bib,outf)
     elif input is not None:
         fn = os.path.splitext(input.groups()[0])[0] + ".tex"
         if fn.count('.') > 1:
             ipdb.set_trace()
-        print ii, "Doing input " + fn
+        print(ii, "Doing input " + fn)
         with open(os.path.join(ppath,fn),'r') as f:
             if 'preface' in line:
-                print >>outf,f.read(),
+                print(f.read(), end='', file=outf)
             else:
-                print >>outf,strip_input(f.readlines()),
+                print(strip_input(f.readlines()), end='', file=outf)
     elif include is not None:
         fn = os.path.splitext(include.groups()[0])[0] + ".tex"
-        print ii, "Doing include " + fn
+        print(ii, "Doing include " + fn)
         f = open(os.path.join(ppath,fn),'r')
         if 'preface' in line:
-            print >>outf,f.read(),
+            print(f.read(), end='', file=outf)
         else:
-            print >>outf,strip_input(f.readlines()),
+            print(strip_input(f.readlines()), end='', file=outf)
         f.close()
     elif bib is not None:
-        print ii, "Doing bib (no solo) " + fn
+        print(ii, "Doing bib (no solo) " + fn)
         dobib(bib,outf)
     else:
-        print >>outf,line,
+        print(line, end="", file=outf, sep="")
+
+print("Spacecount = {0}".format(spacecount))
 
 outf.close()
 file.close()
@@ -122,7 +133,7 @@ includegre = re.compile('includegraphics\[.*\]{{?(.*?)}?}')
 fig_suffixes = "png|eps|pdf"
 lonelygraphics = re.compile('^\s*{{?(.*?(%s)?)}?}' % fig_suffixes)
 
-out_suffix = "eps" if not args.arxiv else "pdf"
+out_suffix = "pdf" if not isarxiv else "pdf"
 out_suffix = 'pdf'
 
 count = 1
@@ -188,7 +199,7 @@ for ii,line in enumerate(file.readlines()):
         prevline = ''
     elif fig1b is not None:
         if 'Two' in line:
-            print "Two in line: ",line
+            print("Two in line: ",line)
             ipdb.set_trace()
         #print "Found solo figure"
         fign = None#fig1b.groups()[0]
@@ -209,18 +220,21 @@ for ii,line in enumerate(file.readlines()):
         #    print "{1}: Found prevline={0}, setting to ''".format(prevline, line.strip())
         prevline = ''
 
+    if line.strip() == "\FigureTwoAA":
+        assert prevline == 'fig2'
+
     input = inputre.search(line)
 
     if fign is not None:
         #DEBUG print "Found fign: %s" % fign
         if fign in ("#1","#2","#3","#4"):
-            print >>outf,line,
+            print(line, end="", file=outf)
             continue
         if len(fign) == 1 or isinstance(fign,basestring):
             figlist = [fign]
         else:
             figlist = fign
-        #DEBUG print "Figlist: ",figlist
+        print("Figlist: ",figlist)
         outline = line
         for fign in figlist:
             fignroot = fign
@@ -248,33 +262,36 @@ for ii,line in enumerate(file.readlines()):
                 fn = ppath+fign
             if os.system('ls %s' % fn) != 0:
                 ipdb.set_trace()
-            print "Converting figure " + fn + " to f%i.%s" % (count,out_suffix)
+            print("Converting figure " + fn + " to f%i.%s" % (count,out_suffix))
             outfig = 'f%i.%s' % (count,out_suffix)
             outpath = os.path.join(ppath, outdir, outfig)
-            if args.arxiv:
+            if isarxiv:
                 rslt = os.system('gs -dSAFER -dBATCH -dNOPAUSE -dAutoRotatePages=/None -dPDFSETTINGS=/screen -sDEVICE=pdfwrite -sOutputFile={1} {0}'.format(fn, outpath))
                 if rslt != 0:
                     ipdb.set_trace()
             else:
                 if os.system('cp %s %s' % (fn, outpath)) != 0:
                     ipdb.set_trace()
-            if args.arxiv:
+            if isarxiv:
                 outline = outline.replace(fignroot,os.path.splitext(outfig)[0])
             else:
+                #print(outline.replace(fignroot,outfig), " vs ", outline.replace(fignroot,os.path.splitext(outfig)[0]))
                 outline = outline.replace(fignroot,outfig)
             count += 1
-        print >>outf,outline,
+        print(outline, end="", file=outf)
     elif input is not None:
         fn = os.path.splitext(input.groups()[0])[0] + ".tex"
-        print "Doing input " + fn
+        print("Doing input " + fn)
         f = open(os.path.join(ppath,fn),'r')
         if 'preface' in line:
-            print >>outf,f.read(),
+            print(f.read(), end="", file=outf)
         else:
-            print >>outf,strip_input(f.readlines()),
+            print(strip_input(f.readlines()), end="", file=outf)
         f.close()
+    elif 'figures/' in line:
+        raise ValueError("Found a line with figures/ in it, but it wasn't converted.")
     else:
-        print >>outf,line,
+        print(line, end="", file=outf)
 
 outf.close()
 file.close()
@@ -300,9 +317,9 @@ if args.texit:
         except IOError:
             pass
     os.chdir(ppath)
-    if args.arxiv:
+    if isarxiv:
         os.system('mv %s/ms.pdf %s_draft%s_arxiv.pdf' % (outdir,paper_name,time.strftime("%m%d",time.localtime())))
     else:
-        os.system('mv %s/ms.pdf %s_draft%s_apj.pdf' % (outdir,paper_name,time.strftime("%m%d",time.localtime())))
+        os.system('mv %s/ms.pdf %s_draft%s_aanda.pdf' % (outdir,paper_name,time.strftime("%m%d",time.localtime())))
 
 os.system('tar --exclude Makefile -czf Ginsburg_APEX_H2CO_CMZ_temmaps_%s_%s.tar.gz %s/ ' % (time.strftime("%m%d",time.localtime()),outtype,outdir))
