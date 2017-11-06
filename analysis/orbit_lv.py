123d4728d69ba95267a6de671d2648015d583752
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-11-06 16:01:23 -0700

    updated orbit lv script

--- analysis/orbit_lv.py
+++ analysis/orbit_lv.py
@@ -1,5 +1,5 @@
 import numpy as np
-from spectral_cube import SpectralCube
+from spectral_cube import SpectralCube, BooleanArrayMask
 import pylab as pl
 from paths import mpath,apath,fpath,molpath,hpath
 from astropy import units as u
@@ -25,17 +25,54 @@ first_switch = np.where(np.diff(coords.l.wrap_at(180*u.deg)) < 0)[0][0]
 latitudes = np.interp(longitudes, coords.l.wrap_at(180*u.deg)[:first_switch+1],
                       coords.b[:first_switch+1], left=np.nan, right=np.nan)
 
-mask = np.zeros(cube.shape[1:], dtype='bool')
-
 pixscale = wcs.utils.proj_plane_pixel_scales(cube.wcs.celestial).mean()*u.deg
-height = int((3*u.arcmin / pixscale).decompose())
+twoarcmin = int((2*u.arcmin / pixscale).decompose())
+threearcmin = int((3*u.arcmin / pixscale).decompose())
+fourarcmin = int((4*u.arcmin / pixscale).decompose())
 
-for lon, lat in zip(longitudes,latitudes):
-    if np.isfinite(lat) and np.isfinite(lon):
-        xpix, ypix = map(int, map(np.round, cube.wcs.celestial.wcs_world2pix(lon, lat, 0)))
-        mask[ypix-height:ypix+height, xpix] = True
+def make_lv(minusheight=threearcmin, plusheight=threearcmin, ):
 
-mcube = cube.with_mask(mask).minimal_subcube(spatial_only=True)
-lv = mcube.sum(axis=1)
+    print("minus = {0}, plus = {1}".format(minusheight, plusheight))
 
-lv.quicklook()
+    mask = np.zeros(cube.shape[1:], dtype='bool')
+
+    for lon, lat in zip(longitudes,latitudes):
+        if np.isfinite(lat) and np.isfinite(lon):
+            xpix, ypix = map(int, map(np.round, cube.wcs.celestial.wcs_world2pix(lon, lat, 0)))
+            mask[ypix-minusheight:ypix+plusheight, xpix] = True
+
+    print("mask sum = {0}".format(mask.sum()))
+
+    mcube = cube.with_mask(BooleanArrayMask(mask,
+                                            wcs=cube.wcs,
+                                            shape=cube.shape)).minimal_subcube(spatial_only=True)
+    lv = mcube.sum(axis=1)
+
+    fig = pl.figure()
+    w = lv.wcs
+    w.wcs.ctype[0] = '_GLONCAR'
+    ax = pl.subplot(projection=w)
+    ax.imshow(lv, interpolation='none', origin='lower', cmap='viridis')
+    ax.set_xlabel('Galactic Longitude [deg]')
+    ax.set_ylabel('$V_{LSR}$ [km s$^{-1}$]')
+    fig.savefig(fpath("lvdiagram_13CO_{0}_to_{1}_pixels.pdf".format(minusheight, plusheight)))
+
+
+    mx = cube.max(axis=0)
+    mx.quicklook()
+    mx.FITSFigure.show_contour(mask.astype('int'), levels=[0.5], colors=['r'])
+    mx.FITSFigure.recenter(0,0,0.8)
+    mx.FITSFigure.savefig(fpath("lvdiagram_cutout_region_13CO_{0}_to_{1}_pixels.pdf".format(minusheight, plusheight)))
+
+    return lv
+
+if __name__=='__main__':
+
+    pl.close('all') # close open figures
+    print("two, three, four arcmin = {0}, {1}, {2} pixels".format(twoarcmin, threearcmin, fourarcmin))
+    lv33 = make_lv(minusheight=threearcmin, plusheight=threearcmin)
+    lv24 = make_lv(minusheight=twoarcmin, plusheight=fourarcmin)
+    lv22 = make_lv(minusheight=twoarcmin, plusheight=twoarcmin)
+    lv22.write(paths.adpath('lv/lvdiagram_13CO_{0}_to_{1}_arcmin.fits'.format(2,2)))
+    lv33.write(paths.adpath('lv/lvdiagram_13CO_{0}_to_{1}_arcmin.fits'.format(3,3)))
+    lv24.write(paths.adpath('lv/lvdiagram_13CO_{0}_to_{1}_arcmin.fits'.format(2,4)))
